933e4a997ffecd1ddc00ff137650cab403e3d7cb
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-05-22 16:07:20 -0500

    :construction: 12 - Class

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,19 +1,24 @@
-<script>
-  import { onMount } from "svelte";
+<!-- This is a module that we can used in all the application to make the API call -->
+<script context="module">
+  export async function preload() {
+    let data = {};
+    const response = await this.fetch(`https://us-central1-pugstagram-co.cloudfunctions.net/data`);
+    data = await response.json();
+    return { data };
+  }
+</script>
 
-  import Header from "../components/Header.svelte";
+<script>
   import Main from "../components/Main.svelte";
   import Timeline from "../components/Timeline.svelte";
   import Sidebar from "../components/Sidebar.svelte";
 
-  let data = {};
-  const API = "https://us-central1-pugstagram-co.cloudfunctions.net/data";
-
-  onMount(async () => {
-    const response = await fetch(API);
-    data = await response.json();
-  });
+  export let data;
 </script>
+
+<svelte:head>
+  <title>Pugstagram</title>
+</svelte:head>
 
 <Main>
   <Timeline posts={data.posts} />
